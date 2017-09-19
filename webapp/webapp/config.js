54e6fd3712c2c249a74ed2436c97360f64fcce12
mamevaz <mamevaz@gmail.com>
2017-09-19 11:56:15 +0200

    subir archivos

---
+++ webapp/webapp/config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'informaticamvm',
+		wp_ws_url : 'https://informaticamvm.000webhostapp.com/wp-appkit-api/informaticamvm',
+		wp_url : 'https://informaticamvm.000webhostapp.com',
+		theme : 'q',
+		version : '0.1',
+		app_title : 'informaticamvm',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : '(;f3R.+u;bORQ]U)7`N7|:,Odg>*l!5^#WTNqmI [~GB $E-WC3xbwvgN;a^|x};',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
