9da350e83da1e76e61079aee5564075385f6901f
marco <marco@bunsenlabs>
2017-09-19 10:29:33 +0200

    aplicacion web

---
+++ webapp/config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '4',
+		wp_ws_url : 'http://192.168.65.128/wordpress/wp-appkit-api/4',
+		wp_url : 'http://192.168.65.128/wordpress',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'aplicacion prueba',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'on',
+		auth_key : '<epJLv)S01b%86}4=<0H%xf~_3s%?X.ibFGV1#[5smyb]}Zs7;8_n}89=QMUuh<s',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
